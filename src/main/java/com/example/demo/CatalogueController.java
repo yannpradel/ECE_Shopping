7be871efb1340a05f2b3dd6b09cc52f81361358a
yannpradel <yann.pradel1@gmail.com>
2023-04-04 23:43:03 +0200

    add bijoux page

--- src/main/java/com/example/demo/CatalogueController.java
+++ src/main/java/com/example/demo/CatalogueController.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,10 +25,18 @@ public class CatalogueController implements Initializable {
     private Button bijouxClick;
 
     @FXML
-    void gotoBijoux(ActionEvent event) {
+    void gotoBijoux(ActionEvent event) throws IOException {
         counter++;
-        System.out.println("aaaaa" + counter);
-        welcomeText.setText("Button Clicked " + counter);
+       // System.out.println("aaaaa" + counter);
+        //welcomeText.setText("Button Clicked " + counter);
+        FXMLLoader load = new FXMLLoader(getClass().getResource("productPage.fxml"));
+        Parent root = load.load();
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
     }
 
     @Override
