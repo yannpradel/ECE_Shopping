65c1aa2aa8b4f5391c4015b531e8ad1e064d00d8
Kimsree <sreevatsaravikanth@yahoo.com>
2023-04-15 00:54:09 +0200

    ajout de javadoc

--- src/main/java/com/example/demo/model/Compte.java
+++ src/main/java/com/example/demo/model/Compte.java
@@ -2,6 +2,10 @@ package com.example.demo.model;
 
 import java.sql.*;
 
+/**
+ * Cette classe représente un compte d'utilisateur dans l'application.
+ */
+
 public class Compte {
     private int id;
     private String firstName;
@@ -12,6 +16,18 @@ public class Compte {
     private String address = "---";
     private int isAdmin;
 
+    /**
+     * Constructeur qui permet de créer un compte avec tous les détails.
+     *
+     * @param id l'identifiant du compte
+     * @param firstName le prénom de l'utilisateur
+     * @param lastName le nom de famille de l'utilisateur
+     * @param email l'adresse e-mail de l'utilisateur
+     * @param password le mot de passe du compte
+     * @param balance le solde du compte
+     * @param address l'adresse de l'utilisateur
+     * @param isAdmin 1 si l'utilisateur est un administrateur, 0 sinon
+     */
     public Compte(int id, String firstName, String lastName, String email, String password, double balance, String address, int isAdmin) {
         this.id = id;
         this.firstName = firstName;
@@ -25,9 +41,13 @@ public class Compte {
         if(this.address == null)
             this.address = "---";
     }
-
-    public Compte (String username, String password)
-    {
+    /**
+     * Constructeur qui permet de créer un compte en utilisant uniquement le nom d'utilisateur et le mot de passe.
+     *
+     * @param username le nom d'utilisateur du compte
+     * @param password le mot de passe du compte
+     */
+    public Compte (String username, String password) {
         this.firstName=username;
         this.password=password;
 
@@ -51,8 +71,14 @@ public class Compte {
             this.address = "---";
     }
 
-    public Compte (String username, String password,int admin)
-    {
+    /**
+     * Constructeur qui permet de créer un compte en utilisant uniquement le nom d'utilisateur, le mot de passe et le statut d'administrateur.
+     *
+     * @param username le nom d'utilisateur du compte
+     * @param password le mot de passe du compte
+     * @param admin 1 si l'utilisateur est un administrateur, 0 sinon
+     */
+    public Compte (String username, String password,int admin) {
         this.firstName=username;
         this.password=password;
         this.isAdmin=admin;
